4c0044d6b686cb4cbdd95481783cd808728f1899
sagebeme <eudicas5k@gmail.com>
2023-07-24 19:33:56 +0300

    returns a list of tuples

--- 0x00-pagination/0-simple_helper_function.py
+++ 0x00-pagination/0-simple_helper_function.py
@@ -4,7 +4,8 @@
 Indexing
 """
 
-def index_range(page:int, page_size:int) -> tuple:
+
+def index_range(page: int, page_size: int) -> tuple:
     """
     Return a tuple of size two containing a start index and an end index
     corresponding to the range of indexes to return in a list for those
