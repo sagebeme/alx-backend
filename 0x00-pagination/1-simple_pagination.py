62e0072ac787e1a256bbe18411991cec5b2f52dd
sagebeme <eudicas5k@gmail.com>
2023-07-24 20:07:29 +0300

    adding class into code and adding a get page function

--- 0x00-pagination/1-simple_pagination.py
+++ 0x00-pagination/1-simple_pagination.py
@@ -1,33 +1,21 @@
+#!/usr/bin/env python3
+
+"""
+indexing
+"""
 import csv
+import math
 from typing import List, Tuple
-
-"""
-Indexing
-"""
-
-
-def index_range(page: int, page_size: int) -> tuple:
-    """
-    Return a tuple of size two containing a start index and an end index
-    corresponding to the range of indexes to return in a list for those
-    particular pagination parameters.
-    """
-    start_index = (page - 1) * page_size
-    end_index = page * page_size
-    return (start_index, end_index)
 
 
 class Server:
     """Server class to paginate a database of popular baby names.
     """
-
     DATA_FILE = "Popular_Baby_Names.csv"
-
 
     def __init__(self):
         self.__dataset = None
 
-    @property
     def dataset(self) -> List[List]:
         """Cached dataset
         """
@@ -39,16 +27,24 @@ class Server:
 
         return self.__dataset
 
+    def index_range(self, page: int, page_size: int) -> Tuple[int, int]:
+        """ a function that can return a tuple"""
+        assert isinstance(page, int) and page > 0
+        assert isinstance(page_size, int) and page_size > 0
+        new_list = []
+        new_list.append((page_size * page) - page_size)
+        new_list.append(page_size * page)
+        return tuple(new_list)
 
     def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
-        """
-        Return the appropriate page of the dataset
-        (i.e. the correct list of rows)
-        """
-        assert isinstance(page, int) and page > 0, "page must be a positive integer"
-        assert isinstance(page_size, int) and page_size > 0, "page_size must be a positive integer"
-
-        start_index, end_index = index_range(page, page_size)
-        if start_index >= len(self.dataset):
+        """ get page contents"""
+        assert isinstance(page, int) and page > 0
+        assert isinstance(page_size, int) and page_size > 0
+        self.dataset()
+        pages = self.index_range(page, page_size)
+        data = []
+        if pages[-1] > len(self.__dataset):
             return []
-        return self.dataset[start_index:end_index]
+        for i in range(pages[0], pages[-1]):
+            data.append(self.__dataset[i])
+        return data
