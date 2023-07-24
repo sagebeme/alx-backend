26baafe434980e5af1a6f75ce4d7039399bdc813
sagebeme <eudicas5k@gmail.com>
2023-07-24 19:59:04 +0300

    adding class into code and adding a get page function

--- 0x00-pagination/1-simple_pagination.py
+++ 0x00-pagination/1-simple_pagination.py
@@ -1,7 +1,8 @@
 import csv
-import math
-from typing import List
-from typing import Tuple
+from typing import List, Tuple
+"""
+indexing
+"""
 
 
 def index_range(page: int, page_size: int) -> Tuple[int, int]:
@@ -12,13 +13,15 @@ def index_range(page: int, page_size: int) -> Tuple[int, int]:
     """
     start_index = (page - 1) * page_size
     end_index = page * page_size
-    return (start_index, end_index)
+    return start_index, end_index
 
 
 class Server:
     """Server class to paginate a database of popular baby names.
     """
+
     DATA_FILE = "Popular_Baby_Names.csv"
+
 
     def __init__(self):
         self.__dataset = None
@@ -34,6 +37,7 @@ class Server:
             self.__dataset = dataset[1:]
 
         return self.__dataset
+
 
     def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
         """
