f5b6802cb067f3d683b746bcb2d95a019ebe201d
sagebeme <eudicas5k@gmail.com>
2023-07-26 19:28:34 +0300

    putting data in the cache and getting data from cache

---
+++ 0x01-caching/0-basic_cache.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""Base dictionry"""
+from base_caching import BaseCaching
+
+
+class BasicCache(BaseCaching):
+    """class extending basecaching"""
+    def put(self, key, item):
+        """Add an item in the cache"""
+        if key is not None and item is not None:
+            self.cache_data[key] = item
+
+    def get(self, key):
+        """Get an item by key"""
+        if key is not None and key in self.cache_data:
+            return self.cache_data[key]
+        return None
