21f37724ad10ab015c5ea9717e39680c5ae7d01a
sagebeme <eudicas5k@gmail.com>
2023-08-01 17:28:00 +0300

    changing stuff

--- 0x02-i18n/1-app.py
+++ 0x02-i18n/1-app.py
@@ -2,31 +2,30 @@
 """using config
 """
 from flask import Flask, render_template
-from flask_babel import Babel, request
-
+from flask_babel import Babel
 
 app = Flask(__name__)
 babel = Babel(app)
 
 
-class Config(object):
+class Config:
     """
     config
     """
-    Languages = ['en', 'fr']
+    LANGUAGES = ["en", "fr"]
     BABEL_DEFAULT_LOCALE = 'en'
     BABEL_DEFAULT_TIMEZONE = 'UTC'
 
-    
+
 app.config.from_object(Config)
 
 
-@app.route("/")
-def home():
+@app.route('/')
+def index():
     """
-    the index page
+    render 1-index.html
     """
-    return render_template("1-index.html")
+    return render_template('1-index.html')
 
 
 if __name__ == '__main__':
